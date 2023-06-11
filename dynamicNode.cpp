ca84c01b133f7c6f29dc8630aa8f8fcaca66be81
srayo16 <srayosikder@gmail.com>
2023-06-11 21:15:26 +0600

    node added

--- dynamicNode.cpp
+++ dynamicNode.cpp
@@ -21,21 +21,6 @@ public:
 
 int main()
 {
-    Node a(10), b(20);
-    a.next = &b;
-
-    // a.val = 10;
-    // b.val = 20;
-
-    // a.next = &b;
-    // b.next = NULL;
-
-    cout << a.val << endl;
-    cout << a.next->val << endl;
-    // cout << (*a.next).val << endl;
-
-    // a.show();
-    // b.show();
 
     return 0;
 }
